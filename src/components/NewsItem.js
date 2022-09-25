d3fad7a79687704bfaa234b486e91ec860ec8fb6
Nisarg <nisargparab8691@gmail.com>
2022-09-25 16:55:16 +0530

    Working app with class based components

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,14 +6,19 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card">
-          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" 
-          style={{left: '90%', zIndex: 1}}>{source}</span>
+
+          <div style={{ display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: '0' }}>
+            <span className="badge rounded-pill bg-danger">{source}</span>
+          </div>
+
           <img src={imageUrl ? imageUrl : "https://m.media-amazon.com/images/M/MV5BNjc2NzRlNzgtYWEzZi00MTYwLTlkNmItYzI5YzdlODRjYjQ0XkEyXkFqcGdeQXVyODgzOTQ0NzA@._V1_.jpg"} className="card-img-top" alt="..." />
+
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
             <p className="card-text"><small className="text-muted">By {author ? author : "Anonymous"} on {new Date(date).toGMTString()}</small></p>
             <a rel="noopener noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+
           </div>
         </div>
       </div>
